d16db0bda43ca179692ca645583775752153bc9d
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-01-26 16:02:08 -0700

    little bittle css

--- superplanner.client/src/components/BoardComponent.vue
+++ superplanner.client/src/components/BoardComponent.vue
@@ -22,5 +22,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.card{
+  width: 24rem;
+  height: 5rem;
+   border-radius: 4px;
+  box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);
+    transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
+}
+.card:hover{
+  border-color: #2069e0;
+  transform: scale(1.05);
+box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+}
 
 </style>
