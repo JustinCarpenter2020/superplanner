77036ab558147e1ebff9115d05b3edc1008123f2
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-01-26 16:20:58 -0700

    board details got info

--- superplanner.client/src/pages/BoardDetailsPage.vue
+++ superplanner.client/src/pages/BoardDetailsPage.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="BoardDetailsPage">
-    <h1>helloooo </h1>
+    <h1>{{ state.board.title }} : </h1>
   </div>
 </template>
 
 <script>
+import { reactive, computed, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { AppState } from '../AppState'
+import { logger } from '../utils/Logger'
+import { boardService } from '../services/BoardService'
 export default {
   name: 'BoardDetailsPage',
   setup() {
-    return {}
+    const route = useRoute()
+    const state = reactive({
+      user: computed(() => AppState.user),
+      board: computed(() => AppState.activeBoard)
+    })
+    onMounted(async() => {
+      try {
+        await boardService.getOne(route.params.id)
+      } catch (error) {
+        logger.error(error)
+      }
+    })
+    return {
+      state
+    }
   },
   components: {}
 }
--- superplanner.client/src/services/BoardService.js
+++ superplanner.client/src/services/BoardService.js
@@ -1,11 +1,17 @@
 import { AppState } from '../AppState'
-// import { logger } from '../utils/Logger'
+import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
 
 class BoardService {
   async getBoards() {
     const res = await api.get('api/boards')
     AppState.boards = res.data
+  }
+
+  async getOne(id) {
+    const res = await api.get('api/boards/' + id)
+    AppState.activeBoard = res.data
+    logger.log(AppState.activeBoard)
   }
 }
 export const boardService = new BoardService()
