315066a5f73e76529f599873205db3a67c974c37
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-01-27 13:56:29 -0700

    list Delete works

--- superplanner.client/src/components/ListComponent.vue
+++ superplanner.client/src/components/ListComponent.vue
@@ -4,11 +4,22 @@
       <div class="card mt-3">
         <div class="card-body">
           <h4 class="card-title">
+            <span class="text-right"><div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle"
+                      type="button"
+                      id="dropdownMenuButton"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+              >
+                <i class="fas fa-align-justify"></i>
+              </button>
+              <div class="dropdown-menu text-danger" aria-labelledby="dropdownMenuButton">
+                <div @click="deleteList" class="dropdown-item" href="#">Delete List</div>
+              </div>
+            </div></span>
             {{ listProp.title }}
           </h4>
-          <p class="card-text">
-            Text
-          </p>
         </div>
       </div>
     </div>
@@ -16,11 +27,23 @@
 </template>
 
 <script>
+import { reactive, computed } from 'vue'
+import { listService } from '../services/ListService'
+import { AppState } from '../AppState'
 export default {
   name: 'ListComponent',
   props: { listProp: { type: Object, required: true } },
-  setup() {
-    return {}
+  setup(props) {
+    const state = reactive({
+      account: computed(() => AppState.account)
+      // editComment: { id: props.commentProp.id }
+    })
+    return {
+      state,
+      deleteList() {
+        listService.deleteList(props.listProp.id)
+      }
+    }
   },
   components: {}
 }
@@ -29,5 +52,9 @@ export default {
 <style lang="scss" scoped>
 .card{
   height: 100%;
+}
+
+.dropdown-toggle::after {
+    content: none;
 }
 </style>
--- superplanner.client/src/services/ListService.js
+++ superplanner.client/src/services/ListService.js
@@ -1,6 +1,7 @@
 import { AppState } from '../AppState'
 import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
+
 class ListService {
   async getLists(id) {
     const res = await api.get('api/boards/' + id + '/lists')
@@ -12,6 +13,12 @@ class ListService {
     const res = await api.post('api/lists/', list)
     AppState.lists.push(res.data)
     return res.send
+  }
+
+  async deleteList(id) {
+    await api.delete('api/lists/' + id)
+    const listInd = AppState.lists.findIndex(l => l.id === id)
+    AppState.lists.splice(listInd, 1)
   }
 }
 export const listService = new ListService()
